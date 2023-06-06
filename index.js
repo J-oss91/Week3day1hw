e41cc743062ac086fa571d81d28309a1b52869df
Jos <58648477+J-oss91@users.noreply.github.com>
2023-06-06 13:25:01 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+//1.
+function maxOfTwoNumbers(x,y){
+  if (x>= y){
+     return x;
+   } else{
+    return y;
+    }
+  }
+  console.log(maxOfTwoNumbers(8,10)
+  )
+//2.
+ function isCharAVowel(Character){
+  if (character == "a","e","i","o","u"){
+         return true}
+          else
+        {return false}
+         console.log(isCharAVowel(Character))}
+//3.
+   const sumArray = function( x ,y, z
+   ){
+    return x + y + z;
+   } 
+  console.log(sumArray(8,7,6))
+//4.
+  function multipleArray(x,y,z){
+  return x * y * z ;
+} console.log( multipleArray(5,7,6))
+//5.
+const maxOfThree = function (
+    x,y,z
+  ){
+    if ((x >= y) && (x >= z)){
+     return x;
+    } else if ((y >= x) && (y >= z)){
+      return y;
+    } else {
+      return z;
+    }
+   }console.log( maxOfThree(7,8,9))
